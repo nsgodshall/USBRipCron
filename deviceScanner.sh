3e2479f0e803c7169c09ae9987109dd65b054e76
Koufax <38049545+nsgodshall@users.noreply.github.com>
2019-09-08 17:46:14 -0700

    Update deviceScanner.sh

--- deviceScanner.sh
+++ deviceScanner.sh
@@ -1,20 +1,17 @@
-authDir=/path/to/auth.json/file
 today=$(date +'%Y-%m-%d')
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-usbrip events violations $authDir -d $today -e -ql > ${dir}/${today}.txt
+$(which usbrip) events history  --manufact "ATMEL AVR" -d $today -e -ql > ${dir}/${today}.txt
 
-if [ -s $dir$today.txt ]
+if [ -s ${dir}/logs/${today}.txt ]
         then
                 echo "To: reciever@example.com" >> ${dir}/email.txt
-                echo "From: 'USBRip' <sender@example.com>" >> ${dir}/email.txt
+                echo "From: 'USBRip' <sender@gmail.com>" >> ${dir}/email.txt
                 echo "Subject: Suspicous USB devices connected to $(hostname) on $today" >> ${dir}/email.txt
                 echo "MIME-Version: 1.0" >> ${dir}/email.txt
                 echo "Content-Type: text/plain" >> ${dir}/email.txt
                 cat  ${dir}/${today}.txt >> ${dir}/email.txt
-                cat ${dir}/email.txt | sendmail -i -t
+                cat ${dir}/email.txt | $(which sendmail) -i -t
                 rm ${dir}/email.txt
         else
                 rm -f ${dir}/${today}.txt
 fi 
-
-
